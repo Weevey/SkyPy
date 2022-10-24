cae1e07493589ca1975c87955bce1e6e70c7c385
Steve Ayres <weevey@hotmail.co.uk>
2022-10-24 10:43:54 +0100

    Added menu pricing

--- tester.py
+++ tester.py
@@ -1,1 +1,7 @@
-print('Pizza')
+print("Pizza - £5 per pizza")
+quantity = int
+quantity = input("How many pizza's would you like? ")
+cost = 5
+total = (quantity * cost)
+
+print("That will be ", total, " pounds please!")
